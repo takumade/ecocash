c623558c9aa49b8ba3bc7d7de4ceec2ca25ceac7
Takunda Madechangu <takundam@sonity.net>
2025-09-08 15:52:02 +0200

    Add barrel file

---
+++ index.js
@@ -0,0 +1,10 @@
+import EcoCash from "./src/ecocash";
+import { InitPaymentResponse, LookupTransactionResponse, RefundDetails, RefundResponse } from "./src/interfaces";
+
+export {
+  EcoCash as default,
+  InitPaymentResponse,
+  LookupTransactionResponse,
+  RefundDetails,
+  RefundResponse
+}

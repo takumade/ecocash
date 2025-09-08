f7bfb9cd8ccff9bb72a7c8800c66337d352321ba
Takunda Madechangu <takundam@sonity.net>
2025-09-08 15:43:07 +0200

    add init payment response

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,3 +1,11 @@
 export interface RefundDetails {
     reference: string, phone: string, amount: number, clientName: string, reason: string, currency?: string 
 }
+
+export interface InitPaymentResponse {
+    phone: string,
+    amount: number,
+    reason: string,
+    currency: string,
+    sourceReference: string
+}

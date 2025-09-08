19203b6cf44f40356ff1e9399ffe7ea438c87565
Takunda Madechangu <takundam@sonity.net>
2025-09-08 14:13:36 +0200

    feat: can refund user

--- src/ecocash.ts
+++ src/ecocash.ts
@@ -1,6 +1,9 @@
 import { randomUUID } from "node:crypto";
 import fetch from "node-fetch";
 
+interface RefundDetails {
+    reference: string, phone: string, amount: number, clientName: string, reason: string, currency?: string 
+}
 
 class EcoCash {
     apiKey: string;
@@ -36,6 +39,30 @@ class EcoCash {
         "reason": reason,
         "currency": "USD",  
         "reference": reference
+      };
+
+      let response = await fetch(url, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(body)
+      });
+
+      return response.json();
+    }
+
+    async refundPayment(details: RefundDetails) {
+      let url = `${this.baseUrl}/api/v2/refund/instant/c2b/${this.mode}`;
+
+      let headers = this.getHeaders();
+
+      let body = {
+        "origionalEcocashTransactionReference": details.reference,
+        "refundCorelator": "012345l61975",
+        "sourceMobileNumber": details.phone,
+        "amount": details.amount,
+        "clientName": details.clientName,
+        "currency": details.currency || "USD",
+        "reasonForRefund": details.reason
       };
 
       let response = await fetch(url, {

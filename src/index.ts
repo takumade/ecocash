c08ad7d9e8a28cb5e52176f9aa877a98c6bfe7e4
Takunda Madechangu <takundam@sonity.net>
2025-09-08 15:56:05 +0200

    clean main file

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
 import EcoCash from "./ecocash";
 
 
+async function example() {
+    let m = new EcoCash("<apiKey>", "<merchant>");
 
-let m = new EcoCash("0XelmqKaarSfHlgftK2vtkwWFRaQoSVh", "08658");
+    //@ts-ignore
+    let response = await m.initPayment("<phone number>", '<amount>', "test");
 
-// m.initPayment("263778548832", 100, "test").then((response) => {
-//     console.log("Response; ", {response});
-// }).catch((error) => {
-//     console.log(error);
-// });
+    let transaction = await m.lookupTransaction(response.sourceReference, response.phone);
 
-m.lookupTransaction("325a802f-943e-47c2-addf-010285f09cea", "263778548832").then((response) => {
     console.log("Response; ", {response});
-}).catch((error) => {
-    console.log(error);
-});
-    
+    console.log("Transaction; ", {transaction});
+}
